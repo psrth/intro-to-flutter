80b490ba85bf0a61612373b01279fed4a13dba79
Parth Sharma <parthsharma151@gmail.com>
2020-07-07 12:42:46 +0530

    app bar and body scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,20 @@ void main() {
   runApp(MyApp());
 }
 
+// // shorthand for one-line main method
+// void main() => runApp(MyApp());
+
 // default class MyApp
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Flutter App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Parth's First Flutter App"),
+        ),
+        body: Text("Hello World."),
+      ),
     );
   }
 }
