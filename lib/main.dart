4286a380f3b3d9ae2ce43a21b767222bb2d451a1
Parth Sharma <parthsharma151@gmail.com>
2020-07-07 13:11:13 +0530

    body widget with text and button column

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,42 @@ void main() {
 
 // default class MyApp
 class MyApp extends StatelessWidget {
+  // creating a stateless widget and building w constructor
   @override
   Widget build(BuildContext context) {
+    // build widget returns a material app consisting of other widgets:
     return MaterialApp(
+      // the home widget consists of a visual scaffold widget for material
+      // design widgets
       home: Scaffold(
+        // standard app bar widget
         appBar: AppBar(
           title: Text("Parth's First Flutter App"),
         ),
-        body: Text("Hello World."),
+
+        // a body widget with a vertical column
+        body: Column(
+          // the children of the column are a list
+          children: <Widget>[
+            Text('The question'),
+            RaisedButton(
+              child: Text('Hello 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Hello 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Hello 3'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Hello 4'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
